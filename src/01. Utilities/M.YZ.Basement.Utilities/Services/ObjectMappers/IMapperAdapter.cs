2833514bc0dd939d6a9e4b085354ce2a32a6aa0b
Mohammad Amin <mohamad.aminyaghoobzadeh@gmail.com>
2022-01-14 12:37:01 +0330

    Add MapTo

--- src/01. Utilities/M.YZ.Basement.Utilities/Services/ObjectMappers/IMapperAdapter.cs
+++ src/01. Utilities/M.YZ.Basement.Utilities/Services/ObjectMappers/IMapperAdapter.cs
@@ -1,5 +1,10 @@
-﻿namespace M.YZ.Basement.Utilities.Services.ObjectMappers;
+﻿using System.Linq.Expressions;
+
+namespace M.YZ.Basement.Utilities.Services.ObjectMappers;
 public interface IMapperAdapter
 {
     TDestination Map<TSource, TDestination>(TSource source);
+
+    IQueryable<TDestination> MapTo<TSource, TDestination>(IQueryable source,
+        params Expression<Func<TDestination, object>>[] membersToExpand);
 }
